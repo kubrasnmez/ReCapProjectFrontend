312d13fe3bc583dd39795ba13a0eb4485098d727
kubrasnmez <sonmezkbra@gmail.com>
2021-03-28 09:46:37 +0300

    carFilterPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
@@ -11,8 +11,8 @@ import { ColorComponent } from './components/color/color.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { RentalComponent } from './components/rental/rental.component';
 import { TabComponent } from './components/tab/tab.component';
-import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { CarDetailComponent } from './components/car/carDetail/car-detail.component';
+import { CarFilterPipePipe } from './pipes/car-filter-pipe.pipe';
 
 
 
@@ -26,13 +26,14 @@ import { CarDetailComponent } from './components/car/carDetail/car-detail.compon
     CustomerComponent,
     RentalComponent,
     TabComponent,
-    VatAddedPipe,
     CarDetailComponent,
+    CarFilterPipePipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,74 +1,4 @@
-/*
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Car } from 'src/app/models/car';
-import { CarDetail } from 'src/app/models/car-detail';
-import { CarImage } from 'src/app/models/carImage';
-import { CarService } from 'src/app/services/car.service';
-import { environment } from 'src/environments/environment';
-import { CarDetailComponent } from './car-detail/car-detail.component';
 
-@Component({
-  selector: 'app-car',
-  templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
-})
-export class CarComponent implements OnInit {
-  cars : Car[] = [];
-  carDetails : CarDetail[] = [];
-  carDetail: CarDetail;
-  carImages : CarImage[] = [];
-  currentCar : Car;
-  imageBasePath = environment.baseURL;
-  apiURL:'https://localhost:44359/api/';
-
-  //private - sadece bu class'da geçerli
-  constructor(private carService : CarService,
-    private activatedRoute : ActivatedRoute) { }
-
-  ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-       if(params["brandId"]){
-        this.getCarsByBrand(params["brandId"])
-      }
-      else if (params["colorId"]){
-        this.getCarsByColor(params["colorId"])
-      }
-      else{
-        this.getCars();
-      }
-    })
-    
-  }
-  
-  getCars(){
-    this.carService.getCars().subscribe(response=>{
-      this.cars = response.data 
-    })
-  }
-  getCarsByBrand(brandId:number){
-    this.carService.getCarsByBrand(brandId).subscribe(response=>{
-      this.cars = response.data 
-    })
-  }
-  getCarsByColor(colorId:number){
-    this.carService.getCarsByColor(colorId).subscribe(response=>{
-      this.cars = response.data
-    })
-  }
-  getCarClass(car:Car){
-    if(car == this.currentCar){
-      return "table-info cursorPointer"
-    }else{
-      return "cursorPointer"
-    }
-  }
-  setCurrentCar(car:Car){
-    this.currentCar=car;
-    
-  }
-}
-*/
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Car } from 'src/app/models/car';
@@ -84,6 +14,7 @@ export class CarComponent implements OnInit {
 
   cars : Car[] = [];
   imageBasePath  = environment.imageUrl;
+  filterText = "";
   
 
   constructor(private carService: CarService,
